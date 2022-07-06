92cd8b19cb2e6e9f4cf4b8282054c623abcd5d5d
DeViktor <contato.regedit@gmail.com>
2022-07-06 18:32:39 -0400

    added js

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Close from './close'
 import './index.css'
+import Java from './java'
+import Javascript from './javascript'
 import Max from './max'
 
 import Minimal from './minimal'
@@ -9,13 +11,6 @@ import Bash from './terminal'
 function App() {
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <h1 className="text-3xl font-bold underline">
-    //       Hello world!
-    //     </h1>
-    //   </header>
-    // </div>
     <body className="bg-gray-900 min-h-screen">
       <header className="pt-4 px-4">
         <div className="bg-slate-600 h-14 flex items-center justify-between rounded-t-xl">
@@ -33,8 +28,17 @@ function App() {
       </header>
       <section className="px-4">
         <div className='min-h-screen mt-0  pt-0 bg-slate-800 border-b border-x border-slate-900'>
-          <div className='text-gray-200 pt-2 pl-1'>
-            <span className='text-green-600'> >_  </span>
+          <div className='pt-2 pl-1'>
+            <div className='animate-write whitespace-nowrap overflow-hidden'>
+            <h1 className='text-green-600'> <span className='animate-ping'> >_ </span> ~~ @DeViktor</h1>
+            <div className='text-green-400'>
+            <p className=''>Hello, My name is João Victor, I'm 22 years old, I've studied Software Development since 2016</p>
+            <p>I've been through languages such as:</p>
+            <div className='flex'>
+            <Java/> <Javascript/>
+            </div>
+            </div>
+            </div>
           </div>
         </div>
       </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        writed:{
+          from: { width: '0'},
+          to:{width: '100%'}
+        }
+      },
+      animation:{
+        write: 'writed 5s 1s normal'
+      }
+    },
   },
   plugins: [],
 }
